ad109e2ad156410ae8e85a7293d5d5dd4f3b6203
Shane R <imspammails@gmail.com>
2021-01-17 21:30:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,12 +4,11 @@ require 'pry'
 def snake_it_up(string)
   if string[0] = "s"
     binding.pry
-    #Using 10.times { - } only returns "10".
+
     #Using "s" + string returns "ssurprise!".
     10.times { string = "s" + string }
     binding.pry
     string
-    #string
   else
     string
   end
