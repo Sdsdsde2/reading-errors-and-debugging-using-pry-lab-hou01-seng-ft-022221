c9a0c5e7fca3efbf9a21c244e56ac73b4471e8d9
Shane R <imspammails@gmail.com>
2021-01-17 21:35:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,10 +1,11 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
+date
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
-
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
@@ -14,5 +15,3 @@ def engage
   puts state_log(date)
   date = generate_star_date
 end
-
-engage
